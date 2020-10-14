e8ab384a523b21201064765822c3ec562b8b32f1
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2020-10-14 21:50:38 +0300

    Update quest_seven.py

--- examples_one/quest_seven.py
+++ examples_one/quest_seven.py
@@ -1,14 +1,13 @@
 import random;
 
-# ageOfHuman = random.randint(-100, 100)
-ageOfHuman = 21
+ageOfHuman = random.randint(-25, 100)
 
 print("Selected:",ageOfHuman)
 
 if ageOfHuman > 0:
     ageOfDog = 0
 
-    if ageOfHuman < 15:
+    if ageOfHuman < 14:
         ageOfDog = ageOfHuman / 7
     elif ageOfHuman < 21:
         ageOfDog = (ageOfHuman / 10.5)
