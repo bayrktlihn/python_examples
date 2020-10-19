89069b0729893a16586329383bc01d164aaf8a1f
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2020-10-19 13:53:34 +0300

    Update quest_06.py

--- examples_three/quest_06.py
+++ examples_three/quest_06.py
@@ -1,19 +1,11 @@
-def get_positive_divides(num):
-    divides = [1]
-    if num < 0:
-        num = -num
-    for i in range(2, (num // 2) + 1):
-        if num % i == 0:
-            divides.append(i)
-    if num != 0:
-        divides.append(num)
-    return divides
-
-
 def is_prime(num):
-    if num < 0:
+    if num < 2:
         return False
-    return len(get_positive_divides(num)) == 2
+    else:
+        for i in range(2, (num // 2) + 1, 2):
+            if num % i == 0:
+                return False
+    return True
 
 
 def find_next_prime(num):
