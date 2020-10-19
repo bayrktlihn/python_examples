6816dfda164a1a1c4398d7c07ffce0d1ec769b4d
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2020-10-19 14:00:25 +0300

    Update quest_01.py

--- examples_three/quest_01.py
+++ examples_three/quest_01.py
@@ -4,7 +4,7 @@ def fix_text(text):
     return " ".join(d)
 
 
-def find_index_begin_space(text: str):
+def find_index_begin_alpha(text: str):
     i = -1
     before_letter = " "
 
@@ -17,9 +17,9 @@ def find_index_begin_space(text: str):
     return i
 
 
-def find_index_last_space(text: str):
+def find_index_last_alpha(text: str):
     text = text[::-1]
-    return (len(text) - 1) - find_index_begin_space(text)
+    return (len(text) - 1) - find_index_begin_alpha(text)
 
 
 def middlestrip(text: str):
@@ -27,8 +27,8 @@ def middlestrip(text: str):
     real_text = fix_text(real_text)
     real_text = real_text.replace(" ", "")
 
-    begin_index = find_index_begin_space(text)
-    end_index = find_index_last_space(text)
+    begin_index = find_index_begin_alpha(text)
+    end_index = find_index_last_alpha(text)
 
     return text[0: begin_index] + real_text + text[end_index + 1:]
 
