d0a269654c655605a38aa8beb104bc1aabeee804
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2020-10-19 13:52:10 +0300

    Update quest_09.py

--- examples_three/quest_09.py
+++ examples_three/quest_09.py
@@ -1,11 +1,21 @@
 def whisper(text):
+    text = text.strip()
     before = " "
     result = ""
+
+    space_count = 1
+
     for letter in text:
+
+        if letter.isspace():
+            space_count += 1
+
         if before.isspace() and letter.isalpha():
             result += letter
-        elif before.isspace() and letter.isspace():
+            space_count = 0
+        elif before.isspace() and letter.isspace() and space_count == 2:
             result += letter
+
         before = letter
     return result.lower()
 
@@ -15,4 +25,3 @@ xx = "A L I H A N  B A Y R A K T A R !"
 result = whisper(xx)
 
 print(result)
-
