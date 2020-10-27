3a6730276175a13c54f232fad4f806eef9cc50fe
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2020-10-27 12:34:28 +0300

    Update q7.py

--- examples_06/q7.py
+++ examples_06/q7.py
@@ -5,20 +5,70 @@ def is_valid_identification_number(val: str):
 
     try:
         val = int(val)
-        return 100000000000 > val > 9999999999
+        if 100000000000 > val > 9999999999:
+            odd_sum_result = sum_odd_numbers(val)
+            even_sum_result = sum_even_numbers(val)
+
+            result = odd_sum_result * 7 - even_sum_result
+
+            ten_digit = get_ten_index(val)
+            last_digit = get_last_digit(val)
+            sum_without_last_digit = sum_digits_without_last_digit(val)
+
+            if result % 10 == ten_digit and last_digit == sum_without_last_digit % 10:
+                return True
+        return False
+
+
     except ValueError as e:
         return False
 
 
+def sum_odd_numbers(val):
+    val //= 100
+    odd_sum_result = 0
+
+    while val > 0:
+        odd_sum_result += val % 10
+        val = val // 100
+
+    return odd_sum_result
+
+
+def sum_even_numbers(val):
+    odd_sum_result = 0
+    val //= 1000
+
+    while val > 0:
+        odd_sum_result += val % 10
+        val = val // 100
+
+    return odd_sum_result
+
+
+def get_ten_index(val):
+    val_str = str(val)
+
+    return int(val_str[9])
+
+
+def get_last_digit(val):
+    val_str = str(val)
+    return int(val_str[10])
+
+
+def sum_digits_without_last_digit(val):
+    val //= 10
+
+    result = 0
+    while val > 0:
+        result += val % 10
+        val //= 10
+
+    return result
+
+
 if __name__ == "__main__":
-    result = is_valid_identification_number("wqeqw")
-
-    print(result)
-
-    result = is_valid_identification_number("900000000000")
-
-    print(result)
-
-    result = is_valid_identification_number("90000000000")
+    result = is_valid_identification_number("50203722674")
 
     print(result)
